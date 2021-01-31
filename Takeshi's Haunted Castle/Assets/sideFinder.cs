ce7d2af20e14075a011d137166adb0f4fd49d626
Kit Heckman <kit.heckman@gmail.com>
2021-01-31 12:49:56 -0500

    enemy drops

---
+++ Takeshi's Haunted Castle/Assets/sideFinder.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sideFinder : MonoBehaviour
+{
+    public GameObject temp;
+    public GameObject[] sides;
+    public GameObject imageF;
+    public GameObject imageB;
+    public GameObject imageL;
+    public GameObject imageR;
+    public float timer;
+    public float time;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sides = GameObject.FindGameObjectsWithTag("sides");
+        
+        //print(temp);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= time)
+        {
+            timer = 0f;
+            float minDist = Mathf.Infinity;
+            Vector3 currentPos = transform.position;
+            foreach (GameObject t in sides)
+            {
+                float dist = Vector3.Distance(t.transform.position, currentPos);
+                if (dist < minDist)
+                {
+                    temp = t;
+                    minDist = dist;
+                }
+            }
+            if (temp.name == "front")
+            {
+                Instantiate(imageF, temp.transform.position, temp.transform.rotation);
+            }
+            else if (temp.name == "back")
+            {
+                Instantiate(imageB, temp.transform.position, temp.transform.rotation);
+            }
+            else if (temp.name == "left")
+            {
+                Instantiate(imageL, temp.transform.position, temp.transform.rotation);
+            }
+            else if (temp.name == "right")
+            {
+                Instantiate(imageR, temp.transform.position, temp.transform.rotation);
+            }
+        }
+    }
+}
